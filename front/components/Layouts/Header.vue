794fb90ea6e99a0be1ac92d41f632c33df70b919
Wesley Ferreira <37143147+WesleyIsFerreira@users.noreply.github.com>
2025-02-18 14:13:13 -0300

    Criando projeto e pastas

---
+++ front/components/Layouts/Header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import logo from '@/assets/svg/logo.svg';
+</script>
+
+<template>
+    <div>
+        <img :src="logo" alt="logo" />
+    </div>
+</template>
---
+++ front/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="ld">
+        <LayoutsHeader />
+        <slot />
+        <LayoutsFooter />
+    </div>
+</template>
+
+<style scoped>
+.ld {
+    background-color: black;
+}
+</style>
